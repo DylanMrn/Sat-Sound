269640150ddbfe568a3fc9a283524a5f126654f3
DylanMrn <dy.marion92@gmail.com>
2023-03-05 02:14:10 +0100

    style first page

--- index.php
+++ index.php
@@ -13,7 +13,36 @@
         h1,h2,h3,h4,h5,h6 {font-family:"Segoe UI",sans-serif}
     </style>
     <header>
-        
+        <div class="container">
+            <div id="start">
+                <div id="logo"> SAT'SOUND </div>
+                <img src="sat.png"/>
+            </div>
+            <div id="middle">
+                <form id="search-form">
+                    <div id="searchbox">
+                        <input id="search" autocapitalize="none" autocomplete="off" autocorrect="off" name="search_query" tabindex="0" type="text" spellcheck="false" placeholder="Rechercher" aria-label="Rechercher">
+                        <button>
+                            <div class="bi bi-play"></div>
+                        </button>
+                    </div>
+                </form>
+            </div>
+            <div id="end">
+                <ul>
+                    <li>
+                        <a href=""> test </a>
+                    </li>
+                    <li>
+                        <a href=""> ahahah </a>
+                    </li>
+                    <div class="button">
+                        <button> S'inscrire </button>
+                        <button> Se connecter </button>
+                    </div>
+                </ul>
+            </div>
+        </div>
     </header>
     <body>
 
