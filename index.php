c66e5d37994a4c3762edd0f8156c2fec7145030c
DylanMrn <dy.marion92@gmail.com>
2023-05-19 00:05:44 +0200

    update 19/05

--- index.php
+++ index.php
@@ -9,11 +9,10 @@
         <link rel="stylesheet" href="css/header.css">
         <link rel="stylesheet" href="owl/js/owl.carousel.min.css">
         <link rel="stylesheet" href="css/carousel.css">
-        <link rel="stylesheet" href="css/test.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
         <script src="js/jquery-3.6.4.min.js"></script>
-        <script src="owl/js/owl.carousel.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
     </head>
     <style>
         html,body {font-family:"Verdana",sans-serif}
@@ -43,131 +42,196 @@
             <div id="end">
                 <ul>
                     <li>
-                        <a href=""> test </a>
+                        <a href="launch.php"> launchpad </a>
                     </li>
                     <li>
                         <a href=""> ahahah </a>
                     </li>
                     <div class="button">
-                        <button> S'inscrire </button>
-                        <button> Se connecter </button>
+                        <div class="register"> 
+                            <a href=''> S'inscrire </a>
+                        </div>
+                        <div class="connect"> 
+                            <a href=''> Se connecter </a>
+                        </div>
                     </div>
                 </ul>
             </div>
         </div>
     </header>
     <section>
-<div id="my-carousel" class="owl-carousel">
-<div class="item">
-  <img src="//via.placeholder.com/1350x250?text=1" alt="" />
- </div>
-<div class="item">
-  <img src="//via.placeholder.com/1350x250?text=2" alt="" />
- </div>
-<div class="item">
-  <img src="//via.placeholder.com/1350x250?text=3" alt="" />
- </div>
-<div class="item">
-  <img src="//via.placeholder.com/1350x250?text=4" alt="" />
- </div>
-<div class="item">
-  <img src="//via.placeholder.com/1350x250?text=5" alt="" />
- </div>
-  </div>
-  
-  
-    <div class="navigation-img-wrapper">
-        <div style="justify-content: space-around;display: flex;">
-            <div class="navigator" data-item="0"> 
-                <div class="content"> 1 </div>
+        <div id="my-carousel" class="owl-carousel">
+            <div class="item">
+                <img src="" alt="" />
             </div>
-            <div class="navigator" data-item="1"> 2 </div>
-            <div class="navigator" data-item="2"> 3 </div>
-            <div class="navigator" data-item="3"> 4 </div>
-            <div class="navigator" data-item="4"> 5 </div>
+            <div class="item">
+                <img src="" alt="" />
+            </div>
+            <div class="item">
+                <img src="gensh.jpg" alt="" style="width: 200px;"/>
+            </div>
         </div>
-    </div>
-</body>
 
+        <div class="navigation-img-wrapper">
+            <div style="justify-content: space-around; display: flex;">
+                <div class="navigator" data-item="0"> 
+                    <div class="content"> <img src="gensh.jpg" alt="" /> </div>
+                </div>
+                <div class="navigator" data-item="1"> 2 </div>
+                <div class="navigator" data-item="2"> 3 </div>
+            </div>
+        </div>
     </section>
     <body>
-      
+        <section>
+            <h6 style="color: white;"> Trend </h6>
+            <div id="card-carousel" class="owl-carousel">
+                <div class="card">
+                    <div class="play"><i class="fas fa-play"></i></div>
+                    <div class="title"> Fart </div>
+                    <div class="sub-title"> it's just the fart song </div>
+                </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+            </div>
+        </section>
+        <section>
+            <h6 style="color: white;"> Foot </h6>
+            <div id="card-carousel-foot" class="owl-carousel">
+                <div class="card">
+                    <div class="play"><i class="fas fa-play"></i></div>
+                    <div class="title"> Fart </div>
+                    <div class="sub-title"> it's just the fart song </div>
+                </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+            </div>
+        </section>
+        <section>
+            <h6 style="color: white;"> Foot </h6>
+            <div id="card-carousel-next" class="owl-carousel">
+                <div class="card">
+                    <div class="play"><i class="fas fa-play"></i></div>
+                    <div class="title"> Fart </div>
+                    <div class="sub-title"> it's just the fart song </div>
+                </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+                <div class="card"> </div>
+            </div>
+        </section>
     </body>
     <footer>
     </footer>
 </html>
 <script>
-    $('.owl-tag').owlCarousel({
-        loop:false,
-        margin:10,
-        nav: true,
-        items:6,
-    });
-
     $(document).ready(function() {
-    
-    $('#my-carousel').owlCarousel({
-        loop : true,
-        autoplay : true,
-        autoplayTimeout:10000,
-        onInitialized: startProgressBar,
-        onTranslate: resetProgressBar,
-        onTranslated: startProgressBar,
-        nav : true,
-        navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>','<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
-        singleItem: true,
-        responsive:{
-            0:{
-            items:1
-            },
-            600:{
-            items:1
-            },
-            1000:{
-            items:1
-            }
-        }
-    });
 
-    $('#my-carousel').on('changed.owl.carousel', function(ev) {
-        var item_index = ev.page.index;
-        $('.navigator').removeClass('active').eq(item_index).addClass('active');
-        $('.navigator').children(".slide-progress").eq(item_index).remove();
-        $('.navigator').eq(item_index).prepend('<div class="slide-progress"></div>');
-
-        console.log($('.navigator'));
-    });
-
-    $('.navigator').on('click', function() {
-        var item_no = $(this).data('item'); 
-        $('#my-carousel').trigger('to.owl.carousel', [item_no, 1000, true]);
+        $('#card-carousel').owlCarousel({
+            loop:false,
+            margin:10,
+            nav: false,
+            items:10,
         });
-    });
 
-    function startProgressBar() {
-    // apply keyframe animation
-    $(".slide-progress").css({
-        width: "100%",
-        transition: "width 10000ms"
-    });
-    }
+        $('#card-carousel-foot').owlCarousel({
+            loop:false,
+            margin:10,
+            nav: false,
+            items:10,
+        });
 
-    function resetProgressBar() {
-    $(".slide-progress").css({
-        width: 0,
-        transition: "width 0s"
-    });
-    }
+        $('#card-carousel-next').owlCarousel({
+            loop:false,
+            margin:10,
+            nav: false,
+            items:10,
+        });
 
-    function progressBar(elem){
-        $elem = elem;
-        buildProgressBar();
-        start();
-    }
+        $('#my-carousel').owlCarousel({
+            loop : true,
+            autoplay : true,
+            autoplayTimeout:10000,
+            onInitialized: startProgressBar,
+            onTranslate: resetProgressBar,
+            onTranslated: startProgressBar,
+            nav : true,
+            navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>','<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
+            singleItem: true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:1
+                },
+                1000:{
+                    items:1
+                }
+            }
+        });
 
-    $('#my-carousel').on('initialized.owl.carousel', function() {
-        $('.navigator').eq(0).addClass('active');
-        $('.navigator').eq(0).prepend('<div class="slide-progress"></div>');
-    });
+        $('#my-carousel').on('changed.owl.carousel', function(ev) {
+            var item_index = ev.page.index;
+            $('.navigator').removeClass('active').eq(item_index).addClass('active');
+            $('.navigator').children(".slide-progress").first().remove();
+            $('.navigator').eq(item_index).prepend('<div class="slide-progress"></div>');
+        });
 
+        $('.navigator').on('click', function() {
+            var item_no = $(this).data('item'); 
+            $('#my-carousel').trigger('to.owl.carousel', [item_no, 1000, true]);
+            });
+        });
+
+        function startProgressBar() {
+        $(".slide-progress").css({
+            width: "100%",
+            transition: "width 10000ms"
+        });
+        }
+
+        function resetProgressBar() {
+        $(".slide-progress").css({
+            width: 0,
+            transition: "width 0s"
+        });
+        }
+
+        function progressBar(elem){
+            $elem = elem;
+            buildProgressBar();
+            start();
+        }
+
+        $('#my-carousel').on('initialized.owl.carousel', function() {
+            $('.navigator').eq(0).addClass('active');
+            $('.navigator').eq(0).prepend('<div class="slide-progress"></div>');
+        });
 </script>
---
+++ launch.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Sat'Sound</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+        <link rel="stylesheet" href="css/header.css">
+        <link rel="stylesheet" href="owl/js/owl.carousel.min.css">
+        <link rel="stylesheet" href="css/carousel.css">
+        <link rel="stylesheet" href="css/launch.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+
+        <script src="js/jquery-3.6.4.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    </head>
+    <style>
+        html,body {font-family:"Verdana",sans-serif}
+        h1,h2,h3,h4,h5,h6 {font-family:"Segoe UI",sans-serif}
+    </style>
+    <header>
+        <div class="container">
+            <div id="start">
+                <div id="logo"> 
+                    <span style="color: #FF1493;"> SAT'</span>  
+                    <span style="color: white;"> SOUND </span> 
+                </div>
+                <img src="sat.png"/>
+            </div>
+            <div id="middle">
+                <div class="searchbar">
+                    <form id="search-form">
+                        <div id="searchbox">
+                            <input id="search" autocapitalize="none" autocomplete="off" autocorrect="off" name="search_query" tabindex="0" type="text" spellcheck="false" placeholder="Rechercher" aria-label="Rechercher">
+                        </div>
+                    </form>
+                    <button>
+                        <div class="bi bi-play"></div>
+                    </button>
+                </div>
+            </div>
+            <div id="end">
+                <ul>
+                    <li>
+                        <a href="launch.php"> launchpad </a>
+                    </li>
+                    <li>
+                        <a href=""> ahahah </a>
+                    </li>
+                    <div class="button">
+                        <button> S'inscrire </button>
+                        <button> Se connecter </button>
+                    </div>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <body>
+        <section>
+            <div class="flex-container">
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>  
+                <div>4</div>
+                <div>5</div>
+                <div>6</div>  
+                <div>7</div>
+                <div>8</div>
+            </div>
+            <div class="flex-container">
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>  
+                <div>4</div>
+                <div>5</div>
+                <div>6</div>  
+                <div>7</div>
+                <div>8</div>
+            </div>
+        </section>
+    </body>
+    <footer>
+    </footer>
+</html>
+<script>
+
+</script>
